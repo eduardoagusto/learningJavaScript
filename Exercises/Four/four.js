28ddbd88e89caf0f41d6c4be8323e52ddb536df9
Eduardo Augusto <84025690+eduardoagusto@users.noreply.github.com>
2023-01-03 13:34:37 -0300

    changing code format

--- Exercises/Four/four.js
+++ Exercises/Four/four.js
@@ -1,7 +1,10 @@
 // Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7.
-function areaTriangle(base,height,lenght){
-    var perimeter = (base+height+lenght)/2;
-    var area = Math.sqrt(perimeter*((perimeter-base)*(perimeter-height)*(perimeter-lenght)));
-    return area;
-  }
-    console.log(areaTriangle(4,5,6));  //9.921567416492215
+function areaTriangle(base, height, lenght) {
+  var perimeter = (base + height + lenght) / 2;
+  var area = Math.sqrt(
+    perimeter *
+      ((perimeter - base) * (perimeter - height) * (perimeter - lenght))
+  );
+  return area;
+}
+console.log(areaTriangle(4, 5, 6)); //9.921567416492215
